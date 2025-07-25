8bca45fc551109e4bc123be389c50242ec86dfff
rockybulislam <rockyislam629@gmail.com>
2025-07-25 20:56:38 +0600

    Fix store import: change from named to default import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-import { store } from "./store.js";
+import store from "./store.js";
 import { router } from "./routePage.jsx";
 import "./index.css";
 
